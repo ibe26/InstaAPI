da750e55d9b36f2ec4c7c7dee91334000c3e5f51
ILKER BERKE ELCHYOGLU <ilkerberke1212@gmail.com>
2023-01-19 10:28:42 +0300

    PostDTO

---
+++ InstaAPI/Model/DTOs/PostBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaAPI.Model
+{
+    public class PostBase
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
